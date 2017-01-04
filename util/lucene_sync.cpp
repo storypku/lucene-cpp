781a9fea8def57afc4cc92ecea258218be172799
liujiaming <liujiaming01@iwaimai.baidu.com>
2017-01-04 12:14:09 +0800

    lucene_sync

---
+++ util/lucene_sync.cpp
@@ -0,0 +1,42 @@
+#include "lucene.h"
+#include "lucene_sync.h"
+#include "synchronize.h"
+#include "lucene_signal.h"
+
+namespace Lucene {
+
+LuceneSync::~LuceneSync() {
+}
+
+SynchronizePtr LuceneSync::get_sync() {
+    Synchronize::create_sync(m_objectLock);
+    return m_objectLock;
+}
+
+LuceneSignalPtr LuceneSync::get_signal() {
+    LuceneSignal::create_signal(m_objectSignal, get_sync());
+    return m_objectSignal;
+}
+
+void LuceneSync::lock(int32_t timeout) {
+    // 忽略timeout参数
+    this->get_sync()->lock();
+}
+
+void LuceneSync::unlock() {
+    this->get_sync()->unlock();
+}
+
+bool LuceneSync::holds_lock() {
+    return this->get_sync()->holds_lock();
+}
+
+void LuceneSync::wait(int32_t timeout) {
+    this->get_signal()->wait(timeout);
+}
+
+void LuceneSync::notify_all() {
+    this->get_signal()->notify_all();
+}
+
+} // namespace Lucene
---
+++ util/lucene_sync.h
@@ -0,0 +1,42 @@
+#ifndef LUCENE_SYNC_H
+#define LUCENE_SYNC_H
+
+#include "lucene.h"
+
+namespace Lucene {
+
+/// Base class for all Lucene synchronize classes
+class LuceneSync {
+public:
+    virtual ~LuceneSync();
+
+protected:
+    SynchronizePtr m_objectLock;
+    LuceneSignalPtr m_objectSignal;
+
+public:
+    /// Return this object synchronize lock.
+    virtual SynchronizePtr get_sync();
+
+    /// Return this object signal.
+    virtual LuceneSignalPtr get_signal();
+
+    /// Lock this object using an optional timeout.
+    virtual bool lock(int32_t timeout = 0);
+
+    /// Unlock this object.
+    virtual void unlock();
+
+    /// Returns true if this object is currently locked by current thread.
+    virtual bool holds_lock();
+
+    /// Wait for signal using an optional timeout.
+    virtual void wait(int32_t timeout = 0);
+
+    /// Notify all threads waiting for signal.
+    virtual void notify_all();
+};
+
+} // namespace Lucene
+
+#endif // LUCENE_SYNC_H
