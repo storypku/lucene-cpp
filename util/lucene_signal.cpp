bc451842181f0d9f3f1d280344f63409d292e6e2
liujiaming <liujiaming01@iwaimai.baidu.com>
2017-01-04 14:28:24 +0800

    bugfix

---
+++ util/lucene_signal.cpp
@@ -0,0 +1,44 @@
+#include "lucene.h"
+#include "lucene_signal.h"
+#include "synchronize.h"
+
+namespace Lucene {
+
+LuceneSignal::LuceneSignal(const SynchronizePtr& objectLock)
+    : m_objectLock(objectLock) {
+}
+
+LuceneSignal::~LuceneSignal() {
+}
+
+void LuceneSignal::create_signal(LuceneSignalPtr& signal, const SynchronizePtr& objectLock) {
+    static std::mutex lockMutex;
+    std::mutex::scoped_lock syncLock(lockMutex);
+    if (!signal) {
+        signal = new_instance<LuceneSignal>(objectLock);
+    }
+}
+
+void LuceneSignal::wait(int32_t timeout) {
+    using Ms = std::chrono::milliseconds;
+
+    int32_t relockCount = m_objectLock ? m_objectLock->unlock_all() : 0;
+    std::mutex::scoped_lock waitLock(m_waitMutex);
+
+    while (std::cv_status::timeout == m_signalCondition.wait_for(waitLock, Ms(timeout))) {
+        if (timeout != 0 || std::cv_status::no_timeout == m_signalCondition.wait_for(waitLock, Ms(10))) {
+            break;
+        }
+    }
+
+    for (int32_t relock = 0; relock < relockCount; ++i) {
+        m_objectLock->lock();
+    }
+}
+
+void LuceneSignal::notify_all() {
+    // 此处的逻辑是难点。
+    m_signalCondition.notify_all();
+}
+
+} // namespace Lucene
---
+++ util/lucene_signal.h
@@ -0,0 +1,36 @@
+#ifndef LUCENE_SIGNAL_H
+#define LUCENE_SIGNAL_H
+
+#include <condition_variable>
+#include <mutex>
+#include "lucene.h"
+
+namespace Lucene {
+
+/// Utility class to support signaling notifications.
+class LuceneSignal {
+public:
+    LuceneSignal(const SynchronizePtr& objectLock = SynchronizePtr());
+
+    virtual ~LuceneSignal();
+
+protected:
+    std::mutex m_waitMutex;
+    std::condition_variable m_signalCondition;
+    SynchronizePtr m_objectLock;
+
+public:
+    /// Create a new LuceneSignal instance atomically.
+    static void create_signal(LuceneSignalPtr& signal, const SynchronizePtr& objectLock);
+
+    /// Wait for signal using an optional timeout.
+    void wait(int32_t timeout = 0);
+
+    /// Notify all threads waiting for signal.
+    void notify_all();
+};
+
+} // namespace Lucene
+
+#endif // LUCENE_SIGNAL_H
+
