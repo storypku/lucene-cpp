5df334a846160f4e25a1ed32ea90d081bf92e3dd
Liu Jiaming <storypku@gmail.com>
2017-01-04 09:48:16 +0800

    bugfix

--- util/synchronize.cpp
+++ util/synchronize.cpp
@@ -8,12 +8,25 @@ namespace Lucene {
 Synchronize::Synchronize() : m_lockThread(0), m_recursionCount(0) {
 }
 
+Synchronize::~Synchronize() {
+}
+
 void Synchronize::create_sync(SynchronizePtr& sync) {
     static boost::mutex lockMutex;
     boost::mutex::scoped_lock syncLock(lockMutex);
     if (!sync) {
-        sync = new_instance<
+        sync = new_instance<Synchronize>();
     }
+}
+
+void Synchronize::lock(int32_t timeout) {
+    if (timeout > 0) {
+        m_mutexSynchronize.timed_lock(boost::posix_time::milliseconds(timeout));
+    } else {
+        m_mutexSynchronize.lock();
+    }
+    m_lockThread = LuceneThread::current_id();
+    ++m_recursionCount;
 }
 
 } // namespace Lucene
