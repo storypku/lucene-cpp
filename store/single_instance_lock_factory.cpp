828c02960d884622dda8f9bfcf7ebfd373049763
Liu Jiaming <storypku@gmail.com>
2017-01-13 02:00:16 +0800

    bugfix

---
+++ store/single_instance_lock_factory.cpp
@@ -0,0 +1,17 @@
+#include "lucene.h"
+#include "single_instance_lock_factory.h"
+
+namespace Lucene {
+
+SingleInstanceLockFactory::SingleInstanceLockFactory()
+    : m_locks(HashSet<String>::new_instance()) {
+}
+
+LockPtr SingleInstanceLockFactory::make_lock(const String& name) {
+    // We do not use the LockPrefix at all, because the private HashSet instance
+    // effectively scopes the locking to this single Directory instance.
+    return new_lucene<SingleInstanceLock>(m_locks, lockName);
+}
+
+} // namespace Lucene
+
--- store/single_instance_lock_factory.h
+++ store/single_instance_lock_factory.h
@@ -5,13 +5,32 @@
 
 namespace Lucene {
 
+/// Implements LockFactory for a single in-process instance, meaning all
+/// locking will take place through this one instance.  Only use this LockFactory
+/// when you are certain all IndexReaders and IndexWriters for a given index are running
+/// against a single shared in-process Directory instance.  This is currently the
+/// default locking for RAMDirectory.
 class SingleInstanceLockFactory : public LockFactory {
 public:
-    SingleInstanceLockFactory() = default;
+    SingleInstanceLockFactory();
     virtual ~SingleInstanceLockFactory() = default;
 
+    LUCENE_CLASS(SingleInstanceLockFactory);
+
+protected:
+    HashSet<String> m_locks;
+
+public:
+    /// Return a new Lock instance identified by lockName.
+    /// @param lockName name of the lock to be created.
+    virtual LockPtr make_lock(const String& lockName);
+
+    /// Attempt to clear (forcefully unlock and remove) the
+    /// specified lock.  Only call this at a time when you are
+    /// certain this lock is no longer in use.
+    /// @param lockName name of the lock to be cleared.
+    virtual void clear_lock(const String& lockName);
 };
 
 } // namespace Lucene
-
 #endif // SINGLE_INSTANCE_LOCK_FACTORY_H
