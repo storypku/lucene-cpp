a99d81ded9432c882125b1cc48618c57c5b856f2
liujiaming <liujiaming01@iwaimai.baidu.com>
2017-01-14 14:31:25 +0800

    bugfix

--- server/main.cpp
+++ server/main.cpp
@@ -2,6 +2,7 @@
 #include "misc_utils.h"
 #include "simple_fs_lock_factory.h"
 #include "directory.h"
+#include "index_input.h"
 #include "info_stream.h"
 #include "ram_output_stream.h"
 #include "ram_directory.h"
@@ -15,10 +16,11 @@ int main(UNUSED int argc, UNUSED char *argv[]) {
     std::cout << "Hello lucene\n";
     RAMDirectoryPtr directory(new_instance<RAMDirectory>());
     IndexOutputPtr out = directory->create_output("hello");
-
     out->write_byte('a');
-    std::cout << out->length() << "\n";
     std::cout << directory->to_string() << "\n";
+    IndexInputPtr in = directory->open_input("hello");
+    uint8_t byte = in->read_byte();
+    std::cout << byte << "\n";
     std::cout << MiscUtils::current_time_millis() << " " << time(NULL) << "\n";
 
     return 0;
