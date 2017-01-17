1b76b9b8c488be6011849432310d970305e275d1
liujiaming <liujiaming01@iwaimai.baidu.com>
2017-01-17 23:18:54 +0800

    bf

--- store/buffered_index_output.cpp
+++ store/buffered_index_output.cpp
@@ -1,7 +1,80 @@
+#include "lucene.h"
 #include "buffered_index_output.h"
+#include "misc_utils.h"
 
 namespace Lucene {
 
 const int32_t BufferedIndexOutput::BUFFER_SIZE = 16384;
+
+BufferedIndexOutput::BufferedIndexOutput()
+    : m_bufferStart(0)
+    , m_bufferPosition(0)
+    , m_buffer(ByteArray::new_instance(BUFFER_SIZE)) {
+
+}
+
+BufferedIndexOutput::~BufferedIndexOutput() {
+}
+
+void BufferedIndexOutput::write_byte(uint8_t b) {
+    if (m_bufferPosition >= BUFFER_SIZE) {
+        flush();
+    }
+    m_buffer[m_bufferPosition++] = b;
+}
+
+void BufferedIndexOutput::write_bytes(const uint8_t* b, int32_t offset, int32_t length) {
+    int32_t bytesLeft = BUFFER_SIZE - m_bufferPosition;
+    if (bytesLeft >= length) {
+        // we add the data to the end of the buffer.
+        MiscUtils::array_copy(b, offset, m_buffer.get(), m_bufferPosition, length);
+        m_bufferPosition += length;
+        // if the buffer is full, flush it.
+        if (BUFFER_SIZE == m_bufferPosition) {
+            flush();
+        }
+    } else if (length > BUFFER_SIZE) {
+        // we flush the buffer.
+        if (m_bufferPosition > 0) {
+            flush();
+        }
+        // and write data at once
+        flush_buffer(b, offset, length);
+        m_bufferStart += length;
+    } else { // bytesLeft < lenth <= BUFFER_SIZE
+        // we fill/flush the buffer (until the input is written);
+        int32_t pos = 0; // position in the input data.
+        int32_t pieceLength;
+        while (pos < length) {
+            pieceLength = (length - pos < bytesLeft) ?  (length - pos) : bytesLeft;
+            MiscUtils::array_copy(b, pos + offset, m_buffer.get(), m_bufferPosition, pieceLength);
+            pos += pieceLength;
+            m_bufferPosition += pieceLength;
+            bytesLeft = BUFFER_SIZE - m_bufferPosition;
+            if (bytesLeft == 0) {
+                flush();
+                bytesLeft = BUFFER_SIZE;
+            }
+        }
+    }
+}
+
+void BufferedIndexOutput::flush() {
+    flush_buffer(m_buffer.get(), m_bufferPosition);
+    m_bufferStart += m_bufferPosition;
+    m_bufferPosition = 0;
+}
+
+void BufferedIndexOutput::flush_buffer(const uint8_t* b, int32_t length) {
+    flush_buffer(b, 0, length);
+}
+
+void BufferedIndexOutput::flush_buffer(const uint8_t* b, int32_t offset, int32_t length) {
+    // override
+}
+
+void BufferedIndexOutput::close() {
+    flush();
+}
 
 }
--- store/buffered_index_output.h
+++ store/buffered_index_output.h
@@ -2,11 +2,64 @@
 #define BUFFERED_INDEX_OUTPUT_H
 
 #include "index_output.h"
+
 namespace Lucene {
 
-class BufferedIndexOutput : public LuceneObject {
+/// Base class implementation for buffered IndexOutput
+class BufferedIndexOutput : public IndexOutput {
+public:
+    BufferedIndexOutput();
+    virtual ~BufferedIndexOutput();
+
+    LUCENE_CLASS(BufferedIndexOutput);
+
 public:
     static const int32_t BUFFER_SIZE;
+
+protected:
+    int64_t m_bufferStart;
+    int32_t m_bufferPosition;
+    ByteArray m_buffer;
+
+public:
+    /// Writes a single byte.
+    virtual void write_byte(uint8_t b);
+
+    /// Writes an array of bytes.
+    /// @param b the bytes to write.
+    /// @param length the number of bytes to write.
+    virtual void write_bytes(const uint8_t* b, int32_t offset, int32_t length);
+
+    /// Forces any buffered output to be written.
+    virtual void flush();
+
+    /// Implements buffer write. Writes bytes at the current position in the
+    /// output.
+    /// @param b the bytes to write.
+    /// @param offset the offset in the byte array.
+    /// @param length the number of bytes to write.
+    virtual void flush_buffer(const uint8_t* b, int32_t offset, int32_t length);
+
+    /// Close this stream.
+    virtual void close();
+
+    /// Returns the current position in this file, where the next write will
+    /// occur.
+    virtual int64_t get_file_pointer();
+
+    /// Set current position in this file, where the next write will occur.
+    virtual void seek(int64_t pos);
+
+    /// The number of bytes in the file.
+    virtual int64_t length() = 0;
+
+protocted:
+    /// Implements buffer write. Writes bytes at the current position in the
+    /// output.
+    /// @param b the bytes to write.
+    /// @param length the number of bytes to write.
+    void flush_buffer(const uint8_t* b, int32_t length);
+
 };
 
 }
