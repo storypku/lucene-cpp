c5def2ed8012f9c7e8a74b87c0209128adc1b252
liujiaming <liujiaming01@iwaimai.baidu.com>
2017-01-23 20:52:28 +0800

    segment_manager

--- server/main.cpp
+++ server/main.cpp
@@ -1,34 +1,73 @@
 #include "lucene.h"
-#include "misc_utils.h"
-#include "simple_fs_lock_factory.h"
-#include "directory.h"
-#include "index_input.h"
-#include "info_stream.h"
-#include "native_fs_lock_factory.h"
-#include "ram_output_stream.h"
-#include "ram_directory.h"
-#include "file_utils.h"
+#include <memory>
+#include <mutex>
 #include <iostream>
-#include <time.h>
+
+namespace Lucene {
+
+class SegmentManager;
+typedef std::shared_ptr<SegmentManager> SegmentManagerPtr;
+
+class SegmentManager : public LuceneObject {
+public:
+    static SegmentManagerPtr get_instance() {
+        static SegmentManagerPtr instance;
+        if (!instance) {
+            instance.reset(new SegmentManager());
+        }
+        return instance;
+    }
+
+protected:
+    std::mutex _crud_mutex;
+    std::mutex _name_mutex;
+    int32_t _segnum_maximum;
+    std::vector<String> _segments;
+
+public:
+    SegmentManager() : _segnum_maximum(0) {
+    }
+
+    String next_avail_name() {
+        int32_t segnum = 0;
+        {
+            std::lock_guard<std::mutex> lock(_name_mutex);
+            segnum = ++_segnum_maximum;
+        }
+        std::cout << "[Retrieval] " << segnum << "\n";
+        return std::to_string(segnum);
+    }
+
+    void add_segment(const String& segment) {
+        std::lock_guard<std::mutex> lock(_crud_mutex);
+        _segments.push_back(segment);
+    }
+
+    void on_merge(const String& segnew, const String& seg1, const String& seg2) {
+        std::lock_guard<std::mutex> lock(_crud_mutex);
+
+        _segments.erase(std::find(_segments.begin(), _segments.end(), seg1));
+        _segments.erase(std::find(_segments.begin(), _segments.end(), seg2));
+
+        _segments.push_back(segnew);
+    }
+
+    void print() {
+        for (const auto& i : _segments) {
+            std::cout << i << "\n";
+        }
+    }
+};
+
+
+}
 
 using namespace Lucene;
-
-int main(UNUSED int argc, UNUSED char* argv[]) {
-    std::cout << "Hello lucene\n";
-    RAMDirectoryPtr directory(new_instance<RAMDirectory>());
-    IndexOutputPtr out = directory->create_output("hello");
-    out->write_byte('a');
-    std::cout << directory->to_string() << "\n";
-    IndexInputPtr in = directory->open_input("hello");
-    uint8_t byte = in->read_byte();
-    std::cout << byte << "\n";
-    NativeFSLockFactoryPtr fsLock = new_instance<NativeFSLockFactory>("indexDir");
-    LockPtr lock = fsLock->make_lock("nativeLock.lock");
-    bool success = lock->obtain();
-    std::cout << "Obtain lock: " << success << "\n";
-    sleep(60);
-    lock->release();
-    fsLock->clear_lock("nativeLock.lock");
-
-    return 0;
+int main() {
+    SegmentManagerPtr instance = SegmentManager::get_instance();
+    instance->add_segment("1");
+    instance->add_segment("2");
+    instance->add_segment("3");
+    instance->on_merge("4", "2", "3");
+    instance->print();
 }
